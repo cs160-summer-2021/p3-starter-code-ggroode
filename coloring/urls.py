585b12e9f3acd3ae648e14f70783a359134b7112
Gavin <ggroode@berkeley.edu>
2021-07-14 13:30:57 -0700

    Final Submission

--- coloring/urls.py
+++ coloring/urls.py
@@ -5,18 +5,15 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    path('demo', views.demo, name='demo'),
-    path('', views.index, name='index'),
-    path('new_interaction', views.index, name='new_interaction'),
-    path('convert/<str:filename>',views.convert,name="convert"),
+    path('', views.homepage, name='index'),
     path('canvas/<int:id>',views.canvas,name='canvas'),
+    path('homepage',views.homepage,name='hompeage'),
     path('homepage/<str:category>', views.homepage, name='homepage'),
     path('canvas/<int:id>/<str:paletteName>',views.canvas,name='canvas'),
     # path('canvas2/<int:id>',views.canvas2,name='canvas2'),
     path('gallery',views.gallery,name="gallery"),
     path('savePalette',views.savePalette,name='savePalette'),
     path('saveImage',views.saveImage,name='saveImage'),
-    path('upload',views.upload,name='upload'),
     path('uploadImage',views.uploadImage,name='uploadImage'),
     path('help',views.help,name='help'),
     path('clear',views.clear,name='clear')
--- coloring/views.py
+++ coloring/views.py
@@ -8,50 +8,18 @@ from .models import Picture, Palette
 from django.conf import settings
 from datetime import datetime
 
-def index(request):
-    return render(request, 'coloring/index.html', {'photos':Picture.objects.all().order_by('-date_modified')})
-def demo(request):
-    return render(request, 'coloring/demo.html')
-
 def help(request):
     return render(request, 'coloring/help.html')
-#<<<<<<< HEAD
-def homepage(request, category):
+
+def homepage(request, category="Mandala"):
     #context = {'pictures': Picture.objects.filter(main=True).order_by('item')}
     categories = list(set([pic.category for pic in Picture.objects.all()]))
     pic1,pic2=Picture.objects.filter(edited=True).order_by('-date_modified')[:2]
     return render(request, 'coloring/homepage.html', {'photos':Picture.objects.filter(category=category),'categories':categories,'category':category,'pic1':pic1,'pic2':pic2})
 
-
-    #return render(request, 'coloring/homepage.html', {'photos':Picture.objects.all()})
-
-#=======
-#def homepage(request,category):
-    #context = {'pictures': Picture.objects.filter(main=True).order_by('item')}
- #   category = list(set([pic.category for pic in Picture.objects.all()]))
- #   return render(request, 'coloring/homepage.html', {'photos':Picture.objects.filter(category=category),'categories':category})
-
-#>>>>>>> e754e7605856b652df5bcab09a54d2926f20b96d
-
-
-#def gallery(request):
- #   pic = Picture.objects.all()[0]
-  #  return render(request,'coloring/gallery.html',context={"pictures":Picture.objects.all()})
-
 def gallery(request):
     pic = Picture.objects.all()[0]
     return render(request,'coloring/gallery.html',context={"pictures":Picture.objects.filter(edited=True).order_by('-date_modified')})
-
-# def canvas(request,id,force=False):
-#     files = os.listdir(STATIC_IMAGE_PATH)
-#     exactMatch = [re.search(str(id)+"\.svg",x) for x in files]
-#     partialmatch = [re.search(str(id)+"\..+",x) for x in files]
-#     if any(exactMatch) and not force:
-#         return render(request,'coloring/canvas.html',{"imagePath":STATIC_IMAGE_PATH_TEMPLATE+str(id)+".svg"})
-#     elif any(partialmatch):
-#         convert(request,list(filter(lambda x: x,partialmatch))[0].string)
-#         return render(request,'coloring/canvas.html',{"imagePath":STATIC_IMAGE_PATH_TEMPLATE+str(id)+".svg"})
-#     return HttpResponse("File does not exist")
 
 def canvas(request,id,paletteName='Basic Colors'):
     pic = Picture.objects.get(pk=id)
@@ -68,12 +36,11 @@ def canvas(request,id,paletteName='Basic Colors'):
         os.system('rm ' + intermediatePath)
         os.system('rm ' + path)
         os.system('rm ' + outputPath)
+        backupName=str(id)+"_backup.svg"
+        os.system("scp " + settings.MEDIA_ROOT+"/"+newName + " " + settings.MEDIA_ROOT + "/" + backupName)
         # else:
         #     os.rename(path,settings.MEDIA_ROOT+"/"+)
-    files = os.listdir(settings.MEDIA_ROOT)
-    backupName=str(id)+"_backup.svg"
-    if all([backupName != f for f in files]):
-        os.system("scp " + settings.MEDIA_ROOT+"/"+newName + " " + settings.MEDIA_ROOT + "/" + backupName)
+
     colors = []
     if paletteName == 'Blank':
         colors =['#FFFFFF']*8
@@ -86,23 +53,6 @@ def canvas(request,id,paletteName='Basic Colors'):
     return render(request,'coloring/canvas.html',{"pic":pic,'paletteName':paletteName,'paletteNames':[p.name for p in Palette.objects.all()],'colors':colors})
 
 
-def upload(request):
-    return render(request,'coloring/upload.html')
-
-STATIC_IMAGE_PATH_TEMPLATE="/static/coloring/images/"
-STATIC_IMAGE_PATH="coloring/static/coloring/images/"
-def convert(request,filename):
-    name,type = filename.split(".")
-    path = STATIC_IMAGE_PATH+filename
-    intermediatePath = STATIC_IMAGE_PATH+name+ ".pgm"
-    outputPath = STATIC_IMAGE_PATH+name+".svg"
-    os.system('convert ' + path + " " + intermediatePath)
-    os.system('potrace ' + intermediatePath +" -s --opaque -o " + outputPath)
-    return HttpResponse("File converted successfully")
-    # image = Image.open(path)
-    # imageArr = np.asarray(image)
-    # bitmap = pt.Bitmap(imageArr)
-    # path = bitmap.trace()
 def savePalette(request):
     paletteName = request.POST.get("paletteName")
     colors = request.POST.get("colors")[:-1]
@@ -131,10 +81,10 @@ def saveImage(request):
         pal.edited=True
         pal.save()
         return HttpResponse('Success')
-
     except Exception as e:
         print(e)
         return HttpResponse('Error')
+
 def uploadImage(request):
     print(request.FILES)
     file = request.FILES['files[]']
